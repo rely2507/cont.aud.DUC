14354e71cc5932d510482d9efe5967698c99489a
rely2507 <cony.cofre.parra90@gmail.com>
2025-07-29 01:24:57 -0400

    Update data.js

--- data.js
+++ data.js
@@ -237,14 +237,6 @@ const asignaturas = [
   },
   {
     semestre: 4,
-    nombre: "Legislación Previsional Y Seguridad Social",
-    sigla: "LPA0201",
-    tipo: "complementaria",
-    prerequisitos: [],
-    creditos: 8
-  },
-  {
-    semestre: 4,
     nombre: "Proceso De Portafolio IV",
     sigla: "APA4123",
     tipo: "especialidad",
@@ -274,7 +266,7 @@ const asignaturas = [
     nombre: "Practica Laboral",
     sigla: "PLA1123",
     tipo: "especialidad",
-    prerequisitos: ["Estados Financieros E IFRS", "Procesos Administrativos De Negocios", "Herramientas Tecnológicas Para La Gestión Contable", "Procedimientos Laborales Y Contratación", "Nivelación Matemática", "Habilidades Básicas De Comunicación", "Proceso De Portafolio I", "Análisis Contable", "Código Tributario", "Remuneraciones", "Algebra", "Habilidades De Comunicación Efectiva", "Fundamentos De Antropología", "Proceso De Portafolio II", "Contabilidades Especiales", "Análisis Económico Para La Administración", "Impuesto A La Renta", "Impuestos A Las Ventas Y Servicios", "Herramientas Financieras", "Mentalidad Emprendedora", "Ingles Básico I", "Doctrina Social De La Iglesia", "Proceso De Portafolio III", "Contabilidad De Inversiones Y Consolidación", "Herramientas Tecnológicas ERP", "Taller De Renta", "Costos, Presupuestos Y Control Presupuestario", "Ingles Básico II", "Ética Para El Trabajo", "Legislación Previsional Y Seguridad Social", "Proceso De Portafolio IV"],
+    prerequisitos: ["Estados Financieros E IFRS", "Procesos Administrativos De Negocios", "Herramientas Tecnológicas Para La Gestión Contable", "Procedimientos Laborales Y Contratación", "Nivelación Matemática", "Habilidades Básicas De Comunicación", "Proceso De Portafolio I", "Análisis Contable", "Código Tributario", "Remuneraciones", "Algebra", "Habilidades De Comunicación Efectiva", "Fundamentos De Antropología", "Proceso De Portafolio II", "Contabilidades Especiales", "Análisis Económico Para La Administración", "Impuesto A La Renta", "Impuestos A Las Ventas Y Servicios", "Herramientas Financieras", "Mentalidad Emprendedora", "Ingles Básico I", "Doctrina Social De La Iglesia", "Proceso De Portafolio III", "Contabilidad De Inversiones Y Consolidación", "Herramientas Tecnológicas ERP", "Taller De Renta", "Costos, Presupuestos Y Control Presupuestario", "Ingles Básico II", "Ética Para El Trabajo", "Proceso De Portafolio IV"],
     creditos: 10
   },
   {
@@ -362,14 +354,6 @@ const asignaturas = [
   },
   {
     semestre: 6,
-    nombre: "Taller De Impuesto A La Renta",
-    sigla: "TIR0010",
-    tipo: "complementaria",
-    prerequisitos: [],
-    creditos: 8
-  },
-  {
-    semestre: 6,
     nombre: "Proceso De Portafolio VI",
     sigla: "APA6123",
     tipo: "especialidad",
@@ -428,14 +412,6 @@ const asignaturas = [
   },
   {
     semestre: 7,
-    nombre: "OPTATIVO",
-    sigla: "XXX0000",
-    tipo: "complementaria",
-    prerequisitos: [],
-    creditos: 2
-  },
-  {
-    semestre: 7,
     nombre: "Proceso De Portafolio Final",
     sigla: "PA71123",
     tipo: "especialidad",
@@ -448,7 +424,7 @@ const asignaturas = [
     nombre: "Portafolio De Titulo",
     sigla: "PTA1123",
     tipo: "especialidad",
-    prerequisitos: ["Estados Financieros E IFRS", "Procesos Administrativos De Negocios", "Herramientas Tecnológicas Para La Gestión Contable", "Procedimientos Laborales Y Contratación", "Nivelación Matemática", "Habilidades Básicas De Comunicación", "Proceso De Portafolio I", "Análisis Contable", "Código Tributario", "Remuneraciones", "Algebra", "Habilidades De Comunicación Efectiva", "Fundamentos De Antropología", "Proceso De Portafolio II", "Contabilidades Especiales", "Análisis Económico Para La Administración", "Impuesto A La Renta", "Impuestos A Las Ventas Y Servicios", "Herramientas Financieras", "Mentalidad Emprendedora", "Ingles Básico I", "Doctrina Social De La Iglesia", "Proceso De Portafolio III", "Contabilidad De Inversiones Y Consolidación", "Herramientas Tecnológicas ERP", "Taller De Renta", "Costos, Presupuestos Y Control Presupuestario", "Ingles Básico II", "Ética Para El Trabajo", "Legislación Previsional Y Seguridad Social", "Proceso De Portafolio IV", "Auditoria Y Control Interno", "Taller Tributario Contable", "Practica Laboral", "Estadística Descriptiva", "Innovación En Procesos", "Ingles Elemental", "Proceso De Portafolio V", "Auditoria De Estados Financieros", "Auditoria Tributaria", "Gobierno Corporativo Y Compliance", "Finanzas Para La Toma De Decisiones", "Ingles Intermedio", "Ética Profesional", "Taller De Impuesto A La Renta", "Proceso De Portafolio VI", "Gestión De Impuestos", "Gestión De Riesgos", "Costos Para La Toma De Decisiones", "Planificación Estratégica Y Control De Gestión", "Evaluación De Proyectos", "ESP Contable", "OPTATIVO", "Proceso De Portafolio Final"],
+    prerequisitos: ["Estados Financieros E IFRS", "Procesos Administrativos De Negocios", "Herramientas Tecnológicas Para La Gestión Contable", "Procedimientos Laborales Y Contratación", "Nivelación Matemática", "Habilidades Básicas De Comunicación", "Proceso De Portafolio I", "Análisis Contable", "Código Tributario", "Remuneraciones", "Algebra", "Habilidades De Comunicación Efectiva", "Fundamentos De Antropología", "Proceso De Portafolio II", "Contabilidades Especiales", "Análisis Económico Para La Administración", "Impuesto A La Renta", "Impuestos A Las Ventas Y Servicios", "Herramientas Financieras", "Mentalidad Emprendedora", "Ingles Básico I", "Doctrina Social De La Iglesia", "Proceso De Portafolio III", "Contabilidad De Inversiones Y Consolidación", "Herramientas Tecnológicas ERP", "Taller De Renta", "Costos, Presupuestos Y Control Presupuestario", "Ingles Básico II", "Ética Para El Trabajo", "Proceso De Portafolio IV", "Auditoria Y Control Interno", "Taller Tributario Contable", "Practica Laboral", "Estadística Descriptiva", "Innovación En Procesos", "Ingles Elemental", "Proceso De Portafolio V", "Auditoria De Estados Financieros", "Auditoria Tributaria", "Gobierno Corporativo Y Compliance", "Finanzas Para La Toma De Decisiones", "Ingles Intermedio", "Ética Profesional", "Proceso De Portafolio VI", "Gestión De Impuestos", "Gestión De Riesgos", "Costos Para La Toma De Decisiones", "Planificación Estratégica Y Control De Gestión", "Evaluación De Proyectos", "ESP Contable", "Proceso De Portafolio Final"],
     creditos: 20
   },
   {
@@ -456,7 +432,7 @@ const asignaturas = [
     nombre: "Practica Profesional",
     sigla: "PPA1123",
     tipo: "especialidad",
-    prerequisitos: ["Estados Financieros E IFRS", "Procesos Administrativos De Negocios", "Herramientas Tecnológicas Para La Gestión Contable", "Procedimientos Laborales Y Contratación", "Nivelación Matemática", "Habilidades Básicas De Comunicación", "Proceso De Portafolio I", "Análisis Contable", "Código Tributario", "Remuneraciones", "Algebra", "Habilidades De Comunicación Efectiva", "Fundamentos De Antropología", "Proceso De Portafolio II", "Contabilidades Especiales", "Análisis Económico Para La Administración", "Impuesto A La Renta", "Impuestos A Las Ventas Y Servicios", "Herramientas Financieras", "Mentalidad Emprendedora", "Ingles Básico I", "Doctrina Social De La Iglesia", "Proceso De Portafolio III", "Contabilidad De Inversiones Y Consolidación", "Herramientas Tecnológicas ERP", "Taller De Renta", "Costos, Presupuestos Y Control Presupuestario", "Ingles Básico II", "Ética Para El Trabajo", "Legislación Previsional Y Seguridad Social", "Proceso De Portafolio IV", "Auditoria Y Control Interno", "Taller Tributario Contable", "Practica Laboral", "Estadística Descriptiva", "Innovación En Procesos", "Ingles Elemental", "Proceso De Portafolio V", "Auditoria De Estados Financieros", "Auditoria Tributaria", "Gobierno Corporativo Y Compliance", "Finanzas Para La Toma De Decisiones", "Ingles Intermedio", "Ética Profesional", "Taller De Impuesto A La Renta", "Proceso De Portafolio VI", "Gestión De Impuestos", "Gestión De Riesgos", "Costos Para La Toma De Decisiones", "Planificación Estratégica Y Control De Gestión", "Evaluación De Proyectos", "ESP Contable", "OPTATIVO", "Proceso De Portafolio Final"],
+    prerequisitos: ["Estados Financieros E IFRS", "Procesos Administrativos De Negocios", "Herramientas Tecnológicas Para La Gestión Contable", "Procedimientos Laborales Y Contratación", "Nivelación Matemática", "Habilidades Básicas De Comunicación", "Proceso De Portafolio I", "Análisis Contable", "Código Tributario", "Remuneraciones", "Algebra", "Habilidades De Comunicación Efectiva", "Fundamentos De Antropología", "Proceso De Portafolio II", "Contabilidades Especiales", "Análisis Económico Para La Administración", "Impuesto A La Renta", "Impuestos A Las Ventas Y Servicios", "Herramientas Financieras", "Mentalidad Emprendedora", "Ingles Básico I", "Doctrina Social De La Iglesia", "Proceso De Portafolio III", "Contabilidad De Inversiones Y Consolidación", "Herramientas Tecnológicas ERP", "Taller De Renta", "Costos, Presupuestos Y Control Presupuestario", "Ingles Básico II", "Ética Para El Trabajo", "Proceso De Portafolio IV", "Auditoria Y Control Interno", "Taller Tributario Contable", "Practica Laboral", "Estadística Descriptiva", "Innovación En Procesos", "Ingles Elemental", "Proceso De Portafolio V", "Auditoria De Estados Financieros", "Auditoria Tributaria", "Gobierno Corporativo Y Compliance", "Finanzas Para La Toma De Decisiones", "Ingles Intermedio", "Ética Profesional", "Proceso De Portafolio VI", "Gestión De Impuestos", "Gestión De Riesgos", "Costos Para La Toma De Decisiones", "Planificación Estratégica Y Control De Gestión", "Evaluación De Proyectos", "ESP Contable", "Proceso De Portafolio Final"],
     creditos: 20
   }
 ];
